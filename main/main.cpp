404ee98d3205efb8862d19a9d37fb692f5c56144
Tyler <wendorfftyler@gmail.com>
2023-03-22 18:44:47 -0400

    Turned main.cpp into a class.

--- main/main.cpp
+++ main/main.cpp
@@ -1,16 +1,40 @@
 #include "log.h"
 #include "../display/displayserver.h"
 
+class Engine
+{
+private:
+    DisplayArguments args;
+    DisplayServer* display_server;
+public:
+    Engine()
+    {
+        LOG("Engine main initialized.");
+        args = {
+            800, 600, "DisplayServer"
+        };
+        display_server = new DisplayServer(args);
+        LOG("Engine initialized.");
+    }
+
+    void mainLoop()
+    {
+        while (!display_server->ShouldDisplayClose()) {
+            display_server->PollDisplayEvents();
+        }
+        this->~Engine();
+    }
+
+    ~Engine()
+    {
+        LOG("Engine display server closed.");
+    }
+};
+
+
 int main()
 {
-    LOG("Engine main initialized.");
-    DisplayArguments args = {
-        800, 600, "DisplayServer"
-    };
-    DisplayServer display_server(args);
-    while (!display_server.ShouldDisplayClose()) {
-        display_server.PollDisplayEvents();
-    }
-    LOG("Engine display server closed.");
+    Engine* engine = new Engine();
+    engine->mainLoop();
     return 0;
 }
